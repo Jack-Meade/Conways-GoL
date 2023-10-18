7562fb4f4c0a1c5d03b8b3c778046b644ffc7c73
Jack-Meade <jack-meade@outlook.com>
2023-10-18 19:12:08 +0100

    Refactor var names

--- src/gol.js
+++ src/gol.js
@@ -2,18 +2,18 @@
 
   const CELL_SIZE = 5;
   let canvas, context, width, height;
-  let grid, rows, columns;
+  let grid, numRows, numColumns;
 
   document.addEventListener('DOMContentLoaded', init, false);
 
   function init() {
-    canvas  = $('canvas')[0];
-    context = canvas.getContext('2d');
-    width   = canvas.width;
-    height  = canvas.height;
-    grid    = createGrid();
-    rows    = grid.length;
-    columns = grid[0].length;
+    canvas     = $('canvas')[0];
+    context    = canvas.getContext('2d');
+    width      = canvas.width;
+    height     = canvas.height;
+    grid       = createGrid();
+    numRows    = grid.length;
+    numColumns = grid[0].length;
     seed();
     window.setInterval(draw, 33);
   }
@@ -28,16 +28,16 @@
 
   function seed() {
     let seededCells = [];
-    for (let i = 0; i < rows; i++) {
+    for (let i = 0; i < numRows; i++) {
       seededCells.push({
-        x: getRandomNumber(1, (rows - CELL_SIZE)) * CELL_SIZE,
-        y: getRandomNumber(1, (columns - CELL_SIZE)) * CELL_SIZE
+        x: getRandomNumber(1, (numRows - CELL_SIZE)) * CELL_SIZE,
+        y: getRandomNumber(1, (numColumns - CELL_SIZE)) * CELL_SIZE
       });
     }
     
     for (let s = 0; s < seededCells.length; s++) {
-      for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < columns; c++) {
+      for (let r = 0; r < numRows; r++) {
+        for (let c = 0; c < numColumns; c++) {
           if (grid[r][c].x === seededCells[s].x && grid[r][c].y === seededCells[s].y) {
             grid[r][c].alive = true;
             grid[r][c+getRandomNumber(-1, 1)].alive = true;
@@ -54,8 +54,8 @@
   function draw() {
     context.clearRect(0, 0, width, height);
     let updatedGrid = createGrid();
-    for (let r = 0; r < rows; r ++) {
-      for (let c = 0; c < columns; c ++) {
+    for (let r = 0; r < numRows; r ++) {
+      for (let c = 0; c < numColumns; c ++) {
         if (grid[r][c].alive) context.fillRect(grid[r][c].x, grid[r][c].y, CELL_SIZE, CELL_SIZE);
         
         switch (getCurrentNeighbourCount(r, c)) {
@@ -70,10 +70,10 @@
 
   function getCurrentNeighbourCount(r, c) {
     let count  = 0;
-    let minRow = (r === 0)           ? 0 : r - 1;
-    let minCol = (c === 0)           ? 0 : c - 1;
-    let maxRow = (r === rows - 1)    ? r : r + 1;
-    let maxCol = (c === columns - 1) ? c : c + 1;
+    let minRow = (r === 0)              ? 0 : r - 1;
+    let minCol = (c === 0)              ? 0 : c - 1;
+    let maxRow = (r === numRows - 1)    ? r : r + 1;
+    let maxCol = (c === numColumns - 1) ? c : c + 1;
     
     for (let checkRow = minRow; checkRow >= minRow && checkRow <= maxRow; checkRow++) {
       for (let checkCol = minCol; checkCol >= minCol && checkCol <= maxCol; checkCol++) {
